f3d285664881ab04af4b92b4cd8a7f85d1f874d2
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-06 15:33:13 +0530

    [Ashika]REFACTORED: Displaying Reset-Password page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import SignUp from './pages/signup/SignUp';
 import SignIn from './pages/signin/SignIn';
 import ForgotEmail from './pages/forgotemail/ForgotEmail';
+import ResetPassword from './pages/resetpassword/ResetPassword';
 
 function App() {
   return (
     <div className="App">
       {/* <SignUp/> */}
       {/* <SignIn/> */}
-      <ForgotEmail/>
+      {/* <ForgotEmail/> */}
+      <ResetPassword/>
     </div>
   );
 }
