a25c34051aa26b0655207a753efeeec0d7d827f5
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-06 15:14:35 +0530

    [Ashika]REFACTORED: Displaying Forgot-Email page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import SignUp from './pages/signup/SignUp';
 import SignIn from './pages/signin/SignIn';
+import ForgotEmail from './pages/forgotemail/ForgotEmail';
 
 function App() {
   return (
     <div className="App">
       {/* <SignUp/> */}
-      <SignIn/>
+      {/* <SignIn/> */}
+      <ForgotEmail/>
     </div>
   );
 }
