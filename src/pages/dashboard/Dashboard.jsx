718b38b91126b12d3d60b0a1311dd23a9ff5c68a
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-07 18:47:21 +0530

    [Ashika]ADDED: Display header component

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from '../../components/header/Header'
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
