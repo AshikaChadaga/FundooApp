353a0c50b9ff8b3ad60dbd8bea2415857d9669df
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-08 15:22:41 +0530

    [Ashika]ADDED: Validation and API to login

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -3,8 +3,62 @@ import Title from '../../components/title/Title'
 import Button from "@mui/material/Button"
 import TextField from '@mui/material/TextField'
 import './SignIn.scss'
+import UserService from '../../service/UserService';
+
+const userService = new UserService();
 
 export class SignIn extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            emailOrPhone: "",
+            password:"",
+            emailOrPhoneError: false,
+            passwordError: false
+        };
+    }
+
+    isValidated = () => {
+        let isError = false;
+        const errors = this.state;
+
+        errors.emailOrPhoneError = this.state.emailOrPhone !== "" ? false : true;
+        errors.passwordError = this.state.password !== "" ? false : true;
+
+        this.setState({
+            ...errors
+        });
+
+        return (isError = errors.emailOrPhoneError || errors.passwordError);
+    };
+
+    validate = () => {
+        var isValid = this.isValidated();
+        console.log(this.state);
+        if (!isValid) {
+            console.log("Validation Successfull!!");
+            let data = {
+                "email": this.state.emailOrPhone,
+                "password": this.state.password
+            };
+            userService.login("/user/login", data)
+                .then(() => {
+                    console.log("User Logged in!")
+                })
+                .catch(error => {
+                    console.error('Error encountered!', error);
+                });
+        }
+    };
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
     render() {
         return (
             <div>
@@ -14,25 +68,79 @@ export class SignIn extends Component {
                     </div>
                     <h1 className="signin-title">Sign in</h1>
                     <h3 className="signin-sub">Use your Fundoo Account</h3>
-                    <div className="form">
+                    <div className="form sign-in-form">
 
                         <div className="name-input">
-                            <TextField fullWidth margin="normal" size="medium" id="Email or phone" label="Email or phone" variant="outlined"/>
-                            <Button style={{ textTransform: 'none', fontWeight: 'bolder', fontSize: '1.5vh', backgroundColor: "#fff", paddingLeft:'0%'}} size="small">Forgot email?</Button>
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                size="medium"
+                                id="Email or phone"
+                                label="Email or phone"
+                                variant="outlined"
+                                name="emailOrPhone"
+                                error={this.state.emailOrPhoneError}
+                                helperText={this.state.emailOrPhoneError ? "Enter email or phone number" : ""}
+                                onChange={(e) => this.changeHandler(e)}
+                            />
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                size="medium"
+                                id="password"
+                                label="Password"
+                                variant="outlined"
+                                name="password"
+                                error={this.state.emailOrPhoneError}
+                                helperText={this.state.emailOrPhoneError ? "Enter your password" : ""}
+                                onChange={(e) => this.changeHandler(e)}
+                            />
+                            <Button
+                                style={{
+                                    textTransform: 'none',
+                                    fontWeight: 'bolder',
+                                    fontSize: '1.5vh',
+                                    backgroundColor: "#fff",
+                                    paddingLeft: '0%'
+                                }}
+                                size="small"
+                            >Forgot email?</Button>
                         </div>
 
                         <div className="learn-section">
                             <p className="helper">Not your computer? Use Guest mode to sign in privately.</p>
-                            <Button style={{ textTransform: 'none', fontWeight: 'bolder', fontSize: '1.5vh', backgroundColor: "#fff", paddingLeft:'0%'}} size="small">Learn more</Button>
+                            <Button
+                                style={{
+                                    textTransform: 'none',
+                                    fontWeight: 'bolder',
+                                    fontSize: '1.5vh',
+                                    backgroundColor: "#fff",
+                                    paddingLeft: '0%'
+                                }}
+                                size="small"
+                            >Learn more</Button>
                         </div>
 
                         <div className="create-account">
-                            <Button style={{textTransform: 'none', fontWeight: 'bolder', fontSize: '1.5vh'}} size="small">Create account</Button>
-                            <Button style={{paddingLeft:'1px', paddingRight:'1px'}} variant="contained" size="medium">Next</Button>
+                            <Button
+                                style={{
+                                    textTransform: 'none',
+                                    fontWeight: 'bolder',
+                                    fontSize: '1.5vh'
+                                }}
+                                size="small"
+                            >Create account</Button>
+                            <Button
+                                style={{
+                                    paddingLeft: '1px',
+                                    paddingRight: '1px'
+                                }}
+                                variant="contained"
+                                size="medium"
+                                onClick={this.validate}
+                            >Next</Button>
                         </div>
-
                     </div>
-
                 </div>
             </div>
         )
