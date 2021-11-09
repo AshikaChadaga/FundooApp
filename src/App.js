0cc954e6db9ba926a2149095a9aa87f0c0362bbe
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-09 21:00:27 +0530

    [Ashika]MODIFIED: Routing paths

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import SignIn from './pages/signin/SignIn';
 import ForgotEmail from './pages/forgotemail/ForgotEmail';
 import ResetPassword from './pages/resetpassword/ResetPassword';
 import Dashboard from './pages/dashboard/Dashboard';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import {Switch} from 'react-router'
 
 function App() {
   return (
-    <div className="App">
-      {/* <SignUp/> */}
-      {/* <SignIn/> */}
-      {/* <ForgotEmail/> */}
-      {/* <ResetPassword/> */}
-      {/* <Dashboard/> */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={SignUp}></Route>
+          <Route exact path='/signin' component={SignIn}></Route>
+          <Route exact path='/forgotemail' component={ForgotEmail}></Route>
+          <Route exact path='/resetpassword' component={ResetPassword}></Route>
+          <Route exact path='/Dashboard' component={Dashboard}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
