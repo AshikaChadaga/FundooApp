1a53686adf164cd7094e31d5fb44b8d47dcd7c09
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-10 23:23:28 +0530

    [Ashika]ADDED: Component to display notes

---
+++ src/components/displaynotes/DisplayNotes.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import UserService from '../../service/UserService';
+import Notes from '../../components/notes/Notes'
+
+const userService = new UserService();
+
+export default function DisplayNotes() {
+    const [notes, setNotes] = useState([]);
+    const displayNote = () => {
+        let config = {
+            headers: {
+                'Authorization': localStorage.getItem("id"),
+            }
+        };
+        userService.displayNotes("/notes/getNotesList", config)
+            .then((res) => {
+                const data = res.data;
+                let notes = data;
+                setNotes(notes.data.data);
+                console.log(notes.data.data);
+                console.log("Notes Displayed!");
+            })
+            .catch(error => {
+                console.error('Error encountered!', error);
+            });
+    }
+
+    useEffect(() => {
+        displayNote();
+    }, []);
+
+    return (
+        <div>
+            <Notes notes={notes}/>
+        </div>
+    )
+}
