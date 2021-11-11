6affa154aa071781a15a966a46dd8c3ff2511615
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-11 12:48:25 +0530

    [Ashika]REFACTORED: Design Style for Icon Buttons

--- src/components/mainnotesicons/MainNotesIcons.jsx
+++ src/components/mainnotesicons/MainNotesIcons.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
-import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import IconButton from "@mui/material/IconButton";
-import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
-import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
-import RedoOutlinedIcon from '@mui/icons-material/RedoOutlined';
-import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Collapse from '@mui/material/Collapse';
 import InputBase from '@mui/material/InputBase';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import UserService from '../../service/UserService';
+import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
+import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
+import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
+import RedoOutlinedIcon from '@mui/icons-material/RedoOutlined';
+import { Button } from '@mui/material';
 
 const userService = new UserService();
 
 function MainNotesIcons(props) {
+    const [reset, setReset] = useState("");
     const [checked, setChecked] = useState(false);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
@@ -54,14 +55,14 @@ function MainNotesIcons(props) {
 
     const bottomIcons = (
         <Box sx={{ display: 'flex' }}>
-            <IconButton><AddAlertOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><PersonAddAlt1OutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><ColorLensOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><ImageOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><ArchiveOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><MoreVertOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><UndoOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
-            <IconButton><RedoOutlinedIcon sx={{ margin: '10px' }} /></IconButton>
+            <IconButton size="small"><AddAlertOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><PersonAddAlt1OutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><ColorLensOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><ImageOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><ArchiveOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><MoreVertOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><UndoOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
+            <IconButton size="small"><RedoOutlinedIcon sx={{ margin: '5px' }} /></IconButton>
             <Box sx={{ flexGrow: 1 }}></Box>
             <Button onClick={saveNote} size="small" sx={{ color: '#5f6368', textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' }}>Close</Button>
         </Box>
@@ -69,6 +70,7 @@ function MainNotesIcons(props) {
     const notestitle = (
         <Box sx={{ display: 'flex' }}>
             <InputBase
+                defaultValue={reset}
                 name="title"
                 placeholder="Title"
                 sx={{ flexGrow: '1' }}
@@ -79,11 +81,12 @@ function MainNotesIcons(props) {
     );
     return (
         <div>
-            <Box sx={{ display: 'flex', flexDirection: 'column', width: '50%', marginLeft: '25%', justifyContent: 'space-between' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '50%', marginLeft: '25vw', justifyContent: 'space-between' }}>
                 <Paper sx={{ padding: '5px 20px 5px 20px', border: '1px solid lightgray', borderRadius: '10px', }}>
                     <Collapse in={checked}>{notestitle}</Collapse>
                     <Box>
                         <InputBase
+                            defaultValue={reset}
                             name="description"
                             placeholder="Take a note... "
                             multiline
