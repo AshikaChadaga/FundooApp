7ff3fe55dcc1f775dad136b69435e2c2c7722a32
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-11 12:49:03 +0530

    [Ashika]REFACTORED: Functinality to update Notes

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -7,33 +7,37 @@ import Fade from '@mui/material/Fade';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import IconButton from "@mui/material/IconButton";
 
-// import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
-// import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
-// import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-// import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
-// import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
-// import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
-// import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
-// import RedoOutlinedIcon from '@mui/icons-material/RedoOutlined';
-// import { Button } from '@mui/material';
-// import Paper from '@mui/material/Paper';
-// import InputBase from '@mui/material/InputBase';
-// import Dialog from "@mui/material/Dialog";
-// import DialogActions from "@mui/material/DialogActions";
-// import { styled } from "@mui/material/styles";
+import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
+import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
+import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
+import RedoOutlinedIcon from '@mui/icons-material/RedoOutlined';
+import { Button } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import { styled } from "@mui/material/styles";
+import UserService from '../../service/UserService';
 
+const userService = new UserService();
 
-// const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-//     "& .MuiDialogContent-root": {
-//         padding: theme.spacing(2)
-//     },
-//     "& .MuiDialogActions-root": {
-//         padding: theme.spacing(1)
-//     }
-// }));
+const bottomIcons = (
+    <Box>
+        <NotesIcon />
+    </Box>
+
+)
 
 export default function Notes(props) {
-    const [hovered, setActive] = React.useState(false);
+    const [hovered, setActive] = useState(false);
+    const [noteid, setNoteId] = useState("");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [open, setOpen] = useState(false);
 
     const active = () => {
         setActive(true);
@@ -41,82 +45,96 @@ export default function Notes(props) {
     const inactive = () => {
         setActive(false);
     }
+    const handleClickOpen = (note) => {
+        setOpen(true);
+        setTitle(note.title);
+        setNoteId(note.id)
+        setDescription(note.description);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-    const bottomIcons = (
-        <Box>
-            <NotesIcon />
+    const updateNote =() => {
+        handleClose();
+        let data = {
+            noteId: noteid,
+            title: title,
+            description: description,
+        }
+        let config = {
+            headers: {
+                'Authorization': localStorage.getItem("id"),
+            }
+        };
+        userService.updateNotes("/notes/updateNotes", data, config)
+        .then(() => {
+            console.log("Notes Updated!");
+            props.displayNote();
+            console.log("Display updated notes called");
+        })
+        .catch(error => {
+            console.error('Error encountered!', error);
+          });
+
+    }
+
+    const bottomPart = (
+        <Box sx={{ display: "flex" }}>
+            <IconButton>
+                <AddAlertOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <PersonAddAlt1OutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <ColorLensOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <ImageOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <ArchiveOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <MoreVertOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <UndoOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <IconButton>
+                <RedoOutlinedIcon sx={{ margin: "5px" }} />
+            </IconButton>
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Button
+                onClick={updateNote}
+                size="small"
+                sx={{
+                    color: "#5f6368",
+                    textTransform: "none",
+                    fontWeight: "bolder",
+                    fontSize: "0.875rem"
+                }}
+            >Close
+            </Button>
         </Box>
+    );
+    const takenotes = (
+        <Box sx={{ display: "flex" }}>
+            <InputBase onChange={(e) => setTitle(e.target.value)} defaultValue={title} placeholder="Title" sx={{ flexGrow: "1" }} />
+            <IconButton>
+                <PushPinOutlinedIcon />
+            </IconButton>
+        </Box>
+    );
 
-    )
-
-    // const [open, setOpen] = React.useState(false);
-
-    // const handleClickOpen = () => {
-    //     setOpen(true);
-    // };
-    // const handleClose = () => {
-    //     setOpen(false);
-    // };
-    // const bottomPart = (
-    //     <Box sx={{ display: "flex" }}>
-    //         <IconButton>
-    //             <AddAlertOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <PersonAddAlt1OutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <ColorLensOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <ImageOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <ArchiveOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <MoreVertOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <UndoOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <IconButton>
-    //             <RedoOutlinedIcon sx={{ margin: "10px" }} />
-    //         </IconButton>
-    //         <Box sx={{ flexGrow: 1 }}></Box>
-    //         <Button
-    //             onClick={handleClose}
-    //             size="small"
-    //             sx={{
-    //                 color: "#5f6368",
-    //                 textTransform: "none",
-    //                 fontWeight: "bolder",
-    //                 fontSize: "0.875rem"
-    //             }}
-    //         >
-    //             Close
-    //         </Button>
-    //     </Box>
-    // );
-    // const takenotes = (
-    //     <Box sx={{ display: "flex" }}>
-    //         <InputBase placeholder="Title" sx={{ flexGrow: "1" }} />
-    //         <IconButton>
-    //             <PushPinOutlinedIcon />
-    //         </IconButton>
-    //     </Box>
-    // );
-
-    return (
-        <div>
-            <div className="main-note">
-                <MainNotesIcons displayNote={props.displayNote}/>
-            </div>
-            <div className="notes">
+    const noteDiv = () =>{
+        return(
+        <div className="notes">
                 {props.notes.map((note) => (
                     <Box onMouseEnter={active} onMouseLeave={inactive}>
-                        <div className="note" >{/*onClick={handleClickOpen}*/}
-                            <div className="note-content">
+                        <div className="note">
+                            <div className="note-content" onClick={() => handleClickOpen(note)}>
                                 <span className="title">{note.title}</span>
                                 <Fade in={hovered}>
                                     <IconButton size="small" >
@@ -129,39 +147,51 @@ export default function Notes(props) {
                                 <Fade in={hovered}>{bottomIcons}</Fade>
                             </div>
                         </div>
-                        {/* <BootstrapDialog onClose={handleClose} open={open}>
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    width: "100%",
-                                    justifyContent: "space-between"
-                                }}
-                            >
-                                <Paper
-                                    sx={{
-                                        padding: "5px 20px 5px 20px",
-                                        border: "1px solid lightgray",
-                                        borderRadius: "10px"
-                                    }}
-                                >
-                                    {takenotes}
-                                    <Box>
-                                        <InputBase
-                                            placeholder="Take a note... "
-                                            multiline
-                                            fullWidth
-                                            sx={{ flexGrow: 1, padding: "20px 0" }}
-                                        />
-                                    </Box>
-                                    <DialogActions>{bottomPart}</DialogActions>
-                                </Paper>
-                            </Box>
-                        </BootstrapDialog> */}
-
                     </Box>
                 ))}
             </div>
+        );
+    }
+
+    return (
+        <div>
+            <div className="main-note">
+                <MainNotesIcons displayNote={props.displayNote} />
+            </div>
+            <div className="notesdiv">
+                {noteDiv()}
+            </div>
+                <Dialog open={open} onClose={handleClose}>
+                <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            width: "100%",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                        <Paper
+                            sx={{
+                                padding: "5px 20px 5px 20px",
+                                border: "1px solid lightgray",
+                                borderRadius: "10px"
+                            }}
+                        >
+                            {takenotes}
+                            <Box>
+                                <InputBase
+                                    defaultValue={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    placeholder="Take a note... "
+                                    multiline
+                                    fullWidth
+                                    sx={{ flexGrow: 1, padding: "20px 0" }}
+                                />
+                            </Box>
+                            <DialogActions>{bottomPart}</DialogActions>
+                        </Paper>
+                    </Box>
+                </Dialog>
         </div>
     )
 }
