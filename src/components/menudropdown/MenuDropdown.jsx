c4d24e36c578b8e9472a0604294277b17df0454a
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-12 08:27:42 +0530

    [Ashika]REFACTORED: Menu dropdown component

---
+++ src/components/menudropdown/MenuDropdown.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import IconButton from "@mui/material/IconButton";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+
+const options = [
+  "Delete note",
+  "Add label",
+  "Add drawing",
+  "Make a copy",
+  "Show checkboxes",
+  "Copy to Google Docs"
+];
+
+export default function MenuDropdown() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <span>
+      <IconButton onClick={handleToggle} ref={anchorRef}>
+        <MoreVertOutlinedIcon sx={{ margin: "5px" }} />
+      </IconButton>
+      <Popper open={open} anchorEl={anchorRef.current} transition style={{ backgroundColor: "#f7f7f7", zIndex: "2000" }}>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement = "bottom-start"
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu">
+                  {options.map((option, index) => (
+                    <MenuItem key={option}>{option}</MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </span>
+  );
+}
