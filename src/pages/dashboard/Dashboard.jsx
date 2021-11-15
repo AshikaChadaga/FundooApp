67b2ba7ff6989ffd934cc9e10aedfb3a7a9ffaf7
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-15 18:13:16 +0530

    [Ashika]REFACTORED: DisplayNotes component called here

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../../components/header/Header'
+import DisplayNotes from '../../components/displaynotes/DisplayNotes'
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <DisplayNotes />
       </div>
     )
   }
