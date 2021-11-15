97bb43eb770cc42ae867d27f124b6d58c7b45683
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-15 18:23:30 +0530

    [Ashika]ADDED: Archived notes page

---
+++ src/pages/archive/Archive.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import DisplayArchive from '../../components/displayArchive/DisplayArchive'
+
+export function Archive() {
+    return (
+        <div>
+            <Header />
+            <DisplayArchive />
+        </div>
+    )
+}
+
+export default Archive
