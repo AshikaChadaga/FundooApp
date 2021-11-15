bad0fe186075e4468ed0db5cfb6be1141f1bf97c
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-15 18:41:04 +0530

    [Ashika]REFACTORED: Routing for Trash notes page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import SignIn from './pages/signin/SignIn';
 import ForgotEmail from './pages/forgotemail/ForgotEmail';
 import ResetPassword from './pages/resetpassword/ResetPassword';
 import Dashboard from './pages/dashboard/Dashboard';
-import Archive from './pages/archive/Archive'
+import Archive from './pages/archive/Archive';
+import Trash from './pages/trash/Trash'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import { Switch } from 'react-router'
 import { ProtectedRoute } from './components/protectedroute/ProtectedRoute';
@@ -20,6 +21,7 @@ function App() {
           <ProtectedRoute path='/resetpassword' component={ResetPassword}></ProtectedRoute>
           <ProtectedRoute exact path='/Dashboard' component={Dashboard}></ProtectedRoute>
           <Route exact path='/dashboard/archive' component={Archive}></Route>
+          <Route exact path='/dashboard/trash' component={Trash}></Route>
           <Route exact path='*' component={()=> "404 Not Found"}></Route>
         </Switch>
       </div>
