ccbd86e52ebae49d9f61b795cab97222cc94faab
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-15 18:41:45 +0530

    [Ashika]REFACTORED: API call to display trash

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -4,35 +4,38 @@ const axiosService = new AxiosService();
 let baseURL = "http://fundoonotes.incubation.bridgelabz.com/api/";
 
 
-class UserService{
-    registration(url, data){
+class UserService {
+    registration(url, data) {
         return axiosService.postMethod(`${baseURL}${url}`, data);
     }
-    login(url, data){
+    login(url, data) {
         return axiosService.postMethod(`${baseURL}${url}`, data);
     }
-    reset(url, data){
+    reset(url, data) {
         return axiosService.postMethod(`${baseURL}${url}`, data);
     }
-    resetPassword(url, data, config){
+    resetPassword(url, data, config) {
         return axiosService.postMethod(`${baseURL}${url}`, data, config);
     }
-    addNotes(url, data, config){
+    addNotes(url, data, config) {
         return axiosService.postMethod(`${baseURL}${url}`, data, config);
     }
-    displayNotes(url, config){
+    displayNotes(url, config) {
         return axiosService.getMethod(`${baseURL}${url}`, config);
     }
-    updateNotes(url, data, config){
+    updateNotes(url, data, config) {
         return axiosService.postMethod(`${baseURL}${url}`, data, config);
     }
-    archive(url, data, config){
+    archive(url, data, config) {
         return axiosService.postMethod(`${baseURL}${url}`, data, config);
     }
-    deleteNote(url, data, config){
+    deleteNote(url, data, config) {
         return axiosService.postMethod(`${baseURL}${url}`, data, config);
     }
-    displayArchivedNotes(url, config){
+    displayArchivedNotes(url, config) {
+        return axiosService.getMethod(`${baseURL}${url}`, config);
+    }
+    displayDeletedNotes(url, config) {
         return axiosService.getMethod(`${baseURL}${url}`, config);
     }
 }
