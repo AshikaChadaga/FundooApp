c05968a81a151458378555d5337836228b5af931
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-15 18:43:08 +0530

    [Ashika]ADDED: Trash notes page

---
+++ src/pages/trash/Trash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import DisplayTrash from '../../components/displaytrash/DisplayTrash';
+
+export function Trash() {
+  return (
+    <div>
+      <Header />
+      <DisplayTrash />
+    </div>
+  )
+}
+
+export default Trash
