ae071db1a966b350edccd9e7e68c9344dfbd8d57
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-16 11:01:32 +0530

    [Ashika]REFACTORED: Created switch routing for trash and archive

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import Header from '../../components/header/Header'
-import DisplayNotes from '../../components/displaynotes/DisplayNotes'
+import React, { Component } from 'react';
+import Header from '../../components/header/Header';
+import DisplayNotes from '../../components/displaynotes/DisplayNotes';
+import Archive from '../archive/Archive';
+import Trash from '../trash/Trash';
 
 export class Dashboard extends Component {
   render() {
@@ -8,6 +10,12 @@ export class Dashboard extends Component {
       <div>
         <Header/>
         <DisplayNotes />
+        <Switch>
+          <ProtectedRoute path='/dashboard' component={Dashboard}></ProtectedRoute>
+          <Route exact path='/dashboard/archive' component={Archive}></Route>
+          <Route exact path='/dashboard/trash' component={Trash}></Route>
+          <Route exact path='*' component={()=> "404 Not Found"}></Route>
+        </Switch>
       </div>
     )
   }
