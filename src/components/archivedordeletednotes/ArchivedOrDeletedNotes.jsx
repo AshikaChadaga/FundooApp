b9c36baa149110a0dffa605269f9e529b537d4c0
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-18 19:05:23 +0530

    [Ashika]REFACTORED: Modified to handle change of color

--- src/components/archivedordeletednotes/ArchivedOrDeletedNotes.jsx
+++ src/components/archivedordeletednotes/ArchivedOrDeletedNotes.jsx
@@ -1,5 +1,149 @@
+// import React, { useEffect, useState } from 'react'
+// import NotesIcon from '../notesicons/NotesIcon';
+// import Box from '@mui/material/Box';
+// import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+// import IconButton from "@mui/material/IconButton";
+// import { Button } from '@mui/material';
+// import Paper from '@mui/material/Paper';
+// import InputBase from '@mui/material/InputBase';
+// import Dialog from "@mui/material/Dialog";
+// import DialogActions from "@mui/material/DialogActions";
+// import UserService from '../../service/UserService';
+// import '../notes/Notes.scss'
+
+// const userService = new UserService();
+
+
+// export default function ArchivedOrDeletedNotes(props) {
+//     const [noteid, setNoteId] = useState("");
+//     const [title, setTitle] = useState("");
+//     const [description, setDescription] = useState("");
+//     const [open, setOpen] = useState(false);
+//     const [color, setColor] = useState("#fff");
+//     const [isArchived, setIsArchived] = useState(false);
+//     const [isDeleted, setIsDeleted] = useState(false);
+
+//     const handleClickOpen = (note) => {
+//         setOpen(true);
+//         setColor(note.color);
+//         setTitle(note.title);
+//         setNoteId(note.id);
+//         setDescription(note.description);
+//     };
+//     const handleClose = () => {
+//         setOpen(false);
+//     };
+
+//     const updateNote = () => {
+//         handleClose();
+//         let data = {
+//             noteId: noteid,
+//             title: title,
+//             description: description,
+//         }
+//         let config = {
+//             headers: {
+//                 'Authorization': localStorage.getItem("id"),
+//             }
+//         };
+//         userService.updateNotes("/notes/updateNotes", data, config)
+//             .then(() => {
+//                 console.log("Notes Updated!");
+//                 props.displayNote();
+//                 console.log("Display updated notes called");
+//             })
+//             .catch(error => {
+//                 console.error('Error encountered While Updating!', error);
+//             });
+
+//     }
+
+//     const takenotes = (
+//         <Box sx={{ display: "flex" }}>
+//             <InputBase onChange={(e) => setTitle(e.target.value)} defaultValue={title} placeholder="Title" sx={{ flexGrow: "1" }} />
+//             <IconButton>
+//                 <PushPinOutlinedIcon />
+//             </IconButton>
+//         </Box>
+//     );
+
+//     const noteDiv = () => {
+//         return (
+//             <div className="notes">
+//                 {props.archive.map((note) => (
+//                     <Box >
+//                         <Paper className="note" sx={{
+//                             border: "1px solid lightgray",
+//                             borderRadius: "10px",
+//                             backgroundColor: note.color,
+//                             padding: "5%"
+//                         }}>
+
+//                             <div className="note-content" onClick={() => handleClickOpen(note)}>
+//                                 <span className="title">{note.title}</span>
+//                                 <span className="pin-icons">
+//                                     <IconButton size="small" >
+//                                         <PushPinOutlinedIcon style={{ color: "#5f6368" }} />
+//                                     </IconButton>
+//                                 </span>
+//                                 <p className="content">{note.description}</p>
+//                             </div>
+//                             <div className="icons">
+//                                 <NotesIcon handleClose={handleClose} setIsDeleted={setIsDeleted} setIsArchived={setIsArchived} displayNote={props.displayNote} setColor={setColor} mode="update" />
+//                             </div>
+//                         </Paper>
+//                     </Box>
+//                 ))}
+//             </div>
+//         );
+//     }
+
+//     return (
+//         <div>
+//             <div className="notesdiv">
+//                 {noteDiv()}
+//             </div>
+//             <Dialog open={open} onClose={handleClose}>
+//                 <Box
+//                     sx={{
+//                         display: "flex",
+//                         flexDirection: "column",
+//                         width: "100%",
+//                         justifyContent: "space-between"
+//                     }}
+//                 >
+//                     <Paper
+//                         sx={{
+//                             padding: "5px 20px 5px 20px",
+//                             backgroundColor: color
+//                         }}
+//                     >
+//                         {takenotes}
+//                         <Box>
+//                             <InputBase
+//                                 defaultValue={description}
+//                                 onChange={(e) => setDescription(e.target.value)}
+//                                 placeholder="Take a note... "
+//                                 multiline
+//                                 fullWidth
+//                                 sx={{ flexGrow: 1, padding: "20px 0" }}
+//                             />
+//                         </Box>
+//                         <DialogActions><Box sx={{ display: 'flex', justifyContent: "space-between" }}>
+//                             <NotesIcon setIsDeleted={setIsDeleted} handleClose={handleClose} setIsArchived={setIsArchived} displayNote={props.displayNote} noteId={noteid} setColor={setColor} mode="update" />
+//                             <Button onClick={updateNote} size="small" sx={{ color: '#5f6368', textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' }}>Close</Button>
+//                         </Box></DialogActions>
+//                     </Paper>
+//                 </Box>
+//             </Dialog>
+//         </div>
+//     )
+// }
+
 import React, { useEffect, useState } from 'react'
 import NotesIcon from '../notesicons/NotesIcon';
+import MainNotesIcons from '../mainnotesicons/MainNotesIcons';
+import '../notes/Notes.scss'
 import Box from '@mui/material/Box';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import IconButton from "@mui/material/IconButton";
@@ -9,33 +153,39 @@ import InputBase from '@mui/material/InputBase';
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import UserService from '../../service/UserService';
-import '../notes/Notes.scss'
+import { deepPurple, blueGrey } from '@mui/material/colors';
+import Avatar from '@mui/material/Avatar';
 
 const userService = new UserService();
 
 
-export default function ArchivedOrDeletedNotes(props) {
+export default function Notes(props) {
     const [noteid, setNoteId] = useState("");
+    const [noteObj, setNoteObj] = useState({});
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [open, setOpen] = useState(false);
     const [color, setColor] = useState("#fff");
+    const [collaborators, setCollaborators] = useState([]);
     const [isArchived, setIsArchived] = useState(false);
     const [isDeleted, setIsDeleted] = useState(false);
 
     const handleClickOpen = (note) => {
         setOpen(true);
+        setNoteObj(note);
         setColor(note.color);
         setTitle(note.title);
         setNoteId(note.id);
         setDescription(note.description);
+        setCollaborators(note.collaborators);
+        // console.log("Notes.collaboratr: ", note.collaborators);
     };
     const handleClose = () => {
         setOpen(false);
     };
 
     const updateNote = () => {
-        handleClose();
+
         let data = {
             noteId: noteid,
             title: title,
@@ -51,6 +201,7 @@ export default function ArchivedOrDeletedNotes(props) {
                 console.log("Notes Updated!");
                 props.displayNote();
                 console.log("Display updated notes called");
+                handleClose();
             })
             .catch(error => {
                 console.error('Error encountered While Updating!', error);
@@ -59,7 +210,7 @@ export default function ArchivedOrDeletedNotes(props) {
     }
 
     const takenotes = (
-        <Box sx={{ display: "flex" }}>
+        <Box sx={{ display: "flex", maxWidth: "1vw" }}>
             <InputBase onChange={(e) => setTitle(e.target.value)} defaultValue={title} placeholder="Title" sx={{ flexGrow: "1" }} />
             <IconButton>
                 <PushPinOutlinedIcon />
@@ -67,18 +218,34 @@ export default function ArchivedOrDeletedNotes(props) {
         </Box>
     );
 
+    const displayCollaborators = (note) => {
+        if (note) {
+
+            if (note.collaborators.length === 0) return null;
+            else {
+                return (
+                    note.collaborators.map((eachCollab) => (
+                        <span><Avatar size="small" sx={{ width: 40, height: 40, bgcolor: blueGrey[400], marginRight: "3px" }}>{eachCollab.firstName.charAt(0)}</Avatar></span>
+                    ))
+                );
+            }
+        }
+        else {
+            return "";
+        }
+    }
+
     const noteDiv = () => {
         return (
             <div className="notes">
                 {props.archive.map((note) => (
-                    <Box >
+                    <Box key={note.id}>
                         <Paper className="note" sx={{
                             border: "1px solid lightgray",
                             borderRadius: "10px",
                             backgroundColor: note.color,
                             padding: "5%"
                         }}>
-
                             <div className="note-content" onClick={() => handleClickOpen(note)}>
                                 <span className="title">{note.title}</span>
                                 <span className="pin-icons">
@@ -87,9 +254,13 @@ export default function ArchivedOrDeletedNotes(props) {
                                     </IconButton>
                                 </span>
                                 <p className="content">{note.description}</p>
+
+                                <Box sx={{ display: 'flex' }}>
+                                    {displayCollaborators(note)}
+                                </Box>
                             </div>
                             <div className="icons">
-                                <NotesIcon handleClose={handleClose} setIsDeleted={setIsDeleted} setIsArchived={setIsArchived} displayNote={props.displayNote} noteId={note.id} setColor={setColor} mode="update" />
+                                <NotesIcon note={note} handleClose={handleClose} setIsDeleted={setIsDeleted} setIsArchived={setIsArchived} displayNote={props.displayNote} setColor={setColor} mode="update" />
                             </div>
                         </Paper>
                     </Box>
@@ -97,7 +268,7 @@ export default function ArchivedOrDeletedNotes(props) {
             </div>
         );
     }
-
+    // console.log("Notes.collaboratr: ", props.note.collaborators);
     return (
         <div>
             <div className="notesdiv">
@@ -129,8 +300,15 @@ export default function ArchivedOrDeletedNotes(props) {
                                 sx={{ flexGrow: 1, padding: "20px 0" }}
                             />
                         </Box>
+                        {
+                            noteObj.collaborators ?
+                                <Box sx={{ display: 'flex' }}>
+                                    {displayCollaborators(noteObj)}
+                                </Box>
+                                : ""
+                        }
                         <DialogActions><Box sx={{ display: 'flex', justifyContent: "space-between" }}>
-                            <NotesIcon setIsDeleted={setIsDeleted} handleClose={handleClose} setIsArchived={setIsArchived} displayNote={props.displayNote} noteId={noteid} setColor={setColor} mode="update" />
+                            <NotesIcon collaborators={collaborators} note={noteObj} setIsDeleted={setIsDeleted} handleClose={handleClose} setIsArchived={setIsArchived} displayNote={props.displayNote} setColor={setColor} mode="update" />
                             <Button onClick={updateNote} size="small" sx={{ color: '#5f6368', textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' }}>Close</Button>
                         </Box></DialogActions>
                     </Paper>
