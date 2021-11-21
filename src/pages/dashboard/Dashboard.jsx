d1d3f423504753cf2d6b984b558721149cdeded1
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-21 09:15:51 +0530

    [Ashika]REFACTORED: Providing context to components

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -7,25 +7,21 @@ import { Route } from 'react-router-dom';
 import { Switch } from 'react-router';
 import SearchPage from '../searchpage/SearchPage';
 import { searchContext } from '../../context/searchContext';
-import { savedNotesContext } from '../../context/savedNotesContext';
 
 export function Dashboard() {
 
   const [searchWord, setSearchWord] = useState("");
-  const [savedNotes, setSavedNotes] = useState([]);
   return (
 
     <div>
       <searchContext.Provider value={[searchWord, setSearchWord]}>
         <Header />
-        <savedNotesContext.Provider value={[savedNotes, setSavedNotes]}>
           <Switch>
             <Route exact path='/dashboard' component={DisplayNotes}></Route>
             <Route exact path='/dashboard/archive' component={Archive}></Route>
             <Route exact path='/dashboard/trash' component={Trash}></Route>
             <Route exact path='/dashboard/search' component={SearchPage}></Route>
           </Switch>
-        </savedNotesContext.Provider>
       </searchContext.Provider>
     </div>
   )
