fb7f6dc98d7aae97fb549fb5392525ec03b38f8b
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-21 09:09:08 +0530

    [Ashika]REFACTORED: Displaying trash and archive notes

--- src/pages/archive/Archive.jsx
+++ src/pages/archive/Archive.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from '../../components/header/Header'
 import DisplayArchive from '../../components/displayArchive/DisplayArchive'
 
 export function Archive() {
     return (
         <div>
-            <Header />
             <DisplayArchive />
         </div>
     )
--- src/pages/trash/Trash.jsx
+++ src/pages/trash/Trash.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from '../../components/header/Header';
 import DisplayTrash from '../../components/displaytrash/DisplayTrash';
 
 export function Trash() {
   return (
     <div>
-      <Header />
       <DisplayTrash />
     </div>
   )
